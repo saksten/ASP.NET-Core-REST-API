8010b5650a735f814a5d0111f33ab9d7e0ed041a
Sakarias Stenbäck <sakarias.stenback@gmail.com>
2021-03-25 19:10:43 +0200

    updates

---
+++ Dtos/CommandUpdateDto.cs
@@ -0,0 +1,12 @@
+namespace WebApi_Application.Dtos
+{
+    public class CommandUpdateDto
+    {
+        public string HowTo { get; set; }
+
+        public string Line { get; set; }
+
+        public string Platform { get; set; }
+
+    }
+}
